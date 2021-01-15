d036e428058ecafe06c41d42287c68cae2e4623f
Adem ÖZCAN <ozcanfurkan@gmail.com>
2021-01-15 18:21:11 +0300

    we don't need to tempServerUrl now

--- lib/network/network_manager.dart
+++ lib/network/network_manager.dart
@@ -4,7 +4,6 @@ import 'package:hag/network/vaccine_response.dart';
 import 'package:http/http.dart' as http;
 
 class NetworkManager {
-  static final String tempServerUrl = 'https://gist.githubusercontent.com/Adem68/0f5a1e138d5a26940d2cd8af6f6631c9/raw/covidasi.json';
   static final String serverUrl = 'https://raw.githubusercontent.com/Quanttant/HAG-Datalog/main/stats.json';
 
   static Future<dynamic> getVaccineStats() async {
