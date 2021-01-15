9251d62f68c5a3d7ba44c50abeb5ae88c42d3583
Adem ÖZCAN <ozcanfurkan@gmail.com>
2021-01-15 14:30:56 +0300

    refresh indicator fixed

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -28,66 +28,74 @@ class _HomeScreenState extends State<HomeScreen> {
                 gradient: LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter, colors: <Color>[Color(0xff18C4B9), Color(0xff02B9C0)])),
           )),
       body: SafeArea(
-        child: SingleChildScrollView(
-          padding: EdgeInsets.symmetric(horizontal: 10.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 40,
-              ),
-              futureBuilder(),
-              SizedBox(
-                height: 40,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    gradient: LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter, colors: <Color>[Color(0xff18C4B9), Color(0xff02B9C0)]),
-                  ),
-                  child: FlatButton(
-                    shape: RoundedRectangleBorder(
+        child: RefreshIndicator(
+          color: Colors.white,
+          backgroundColor: Color(0xff00B8C0),
+          onRefresh: () async {
+            setState(() {});
+          },
+          child: SingleChildScrollView(
+            physics: AlwaysScrollableScrollPhysics(),
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 40,
+                ),
+                futureBuilder(),
+                SizedBox(
+                  height: 40,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 60,
+                    decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(20),
+                      gradient: LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter, colors: <Color>[Color(0xff18C4B9), Color(0xff02B9C0)]),
                     ),
-                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                    child: Text(
-                      'Aşı Grubu Hesaplama',
-                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+                    child: FlatButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      child: Text(
+                        'Aşı Grubu Hesaplama',
+                        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                      onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => CalculationPage())),
                     ),
-                    onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => CalculationPage())),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Color(0xffF8B133),
-                  ),
-                  child: FlatButton(
-                    shape: RoundedRectangleBorder(
+                SizedBox(
+                  height: 40,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 60,
+                    decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(20),
+                      color: Color(0xffF8B133),
                     ),
-                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                    child: Text(
-                      'Ayarlar',
-                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+                    child: FlatButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      child: Text(
+                        'Ayarlar',
+                        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                      onPressed: null,
                     ),
-                    onPressed: null,
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -131,56 +139,60 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget futureBuilder() {
-    //TODO: move refresh indicator to page body because when no internet you can't refresh
-    return RefreshIndicator(
-        color: Colors.white,
-        backgroundColor: Color(0xff00B8C0),
-        onRefresh: () async {
-          setState(() {});
-        },
-        child: SingleChildScrollView(
-          physics: AlwaysScrollableScrollPhysics(),
-          child: FutureBuilder<VaccineReponse>(
-            builder: (context, snapshot) {
-              switch (snapshot.connectionState) {
-                case ConnectionState.none:
-                  return Center(
-                    child: Text('İnternet bağlatınız yok.'),
-                  );
-                case ConnectionState.waiting:
-                  return Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('Veriler yükleniyor...'),
-                        SizedBox(
-                          height: 50,
-                        ),
-                        CircularProgressIndicator(),
-                      ],
-                    ),
-                  );
-                  break;
-                default:
-                  if (snapshot.hasData && !snapshot.hasError) {
-                    return Column(
-                      children: [
-                        cardWidget('Toplam Aşılanan Kişi Sayısı', snapshot.data.total),
-                        SizedBox(
-                          height: 40,
-                        ),
-                        cardWidget('Bugün Aşılanan Kişi Sayısı', snapshot.data.today),
-                      ],
-                    );
-                  } else
-                    return Center(
-                      child: Text('İnternet bağlantısı yok'),
-                    );
-              }
-            },
-            future: getStats(),
+  Widget noInternet() {
+    return Center(
+      child: Column(
+        children: [
+          Icon(
+            Icons.warning,
+            color: Colors.red,
+            size: 48,
           ),
-        ));
+          SizedBox(
+            height: 20,
+          ),
+          Text('İnternet bağlantınız yok.'),
+        ],
+      ),
+    );
+  }
+
+  Widget futureBuilder() {
+    return FutureBuilder<VaccineReponse>(
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+            return noInternet();
+          case ConnectionState.waiting:
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Veriler yükleniyor...'),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  CircularProgressIndicator(),
+                ],
+              ),
+            );
+            break;
+          default:
+            if (snapshot.hasData && !snapshot.hasError) {
+              return Column(
+                children: [
+                  cardWidget('Toplam Aşılanan Kişi Sayısı', snapshot.data.total),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  cardWidget('Bugün Aşılanan Kişi Sayısı', snapshot.data.today),
+                ],
+              );
+            } else
+              return noInternet();
+        }
+      },
+      future: getStats(),
+    );
   }
 }
