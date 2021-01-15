0213aeb9c4992d54a8b72dfc2cd8537fc694f731
Adem ÖZCAN <ozcanfurkan@gmail.com>
2021-01-15 03:31:06 +0300

    hotfix

--- lib/network/network_manager.dart
+++ lib/network/network_manager.dart
@@ -4,12 +4,12 @@ import 'package:hag/network/vaccine_response.dart';
 import 'package:http/http.dart' as http;
 
 class NetworkManager {
-  static final String serverUrl =
-      'https://gist.githubusercontent.com/Adem68/0f5a1e138d5a26940d2cd8af6f6631c9/raw/494bd86fbefd3f2d982aa00fc9ff9898c86e7189/covidasi.json';
+  static final String tempServerUrl = 'https://gist.githubusercontent.com/Adem68/0f5a1e138d5a26940d2cd8af6f6631c9/raw/covidasi.json';
+  static final String serverUrl = 'https://raw.githubusercontent.com/Quanttant/HAG-Datalog/main/stats.json';
 
   static Future<dynamic> getVaccineStats() async {
     try {
-      final response = await http.get(serverUrl);
+      final response = await http.get(tempServerUrl);
       if (response.statusCode == 200) {
         return VaccineReponse.fromJson(json.decode(response.body));
       } else {
